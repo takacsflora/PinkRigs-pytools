aa6a910e85a2c0cb0194c4cb4579c2b977ca040a
takacsflora <40361594+takacsflora@users.noreply.github.com>
2024-11-07 14:41:34 +0000

    fix install

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Flora Takacs',
     author_email='flora.takacs.15@ucl.ac.uk',
     url='https://github.com/takacsflora/floras-helpers',  
-    packages=find_packages(), 
+    packages=find_packages(include = ['pinkrigs_tools', 'pinkrigs_tools.*']), 
     install_requires=[
         "floras_helpers @ git+https://github.com/takacsflora/floras-helpers.git@main#egg=floras_helpers", 
         'ipykernel',
